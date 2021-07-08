06e1d7f2beccb2e9506805d2200dcb2ec186ac52
Pratt Vaidya <pratt.vaidya@gmail.com>
2021-07-08 15:09:27 -0700

    create a mutation to create a User

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -129,8 +129,33 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+// Mutations
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        createUser: {
+            type: UserType,
+            args: {
+                // id: {type: GraphQLID},
+                name: {type: GraphQLString},
+                age: {type: GraphQLInt},
+                profession: {type: GraphQLString},
+            },
+            resolve(parent, args) {
+                let user = {
+                    name: args.name,
+                    age: args.age,
+                    profession: args.profession,
+                }
+                return user
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 })
 
 // GraphiQL queries
@@ -161,3 +186,18 @@ module.exports = new GraphQLSchema({
   }
 }
 */
+
+// GraphQL mutation
+/**
+ mutation {
+  createUser(name: "Apollo", age: 27, profession: "Very smart person") {
+    id
+    name
+    age
+    profession
+    hobbies {
+      id
+    }
+  }
+}
+ */
