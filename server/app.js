21f5cb7e0c66a9fcde9f94677584b41230c58945
Pratt Vaidya <pratt.vaidya@gmail.com>
2021-07-08 21:47:55 -0700

    use cors

--- server/app.js
+++ server/app.js
@@ -9,9 +9,11 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 mongoose.connection.once('open', () => {
     console.log('Connected to Mongo');
 })
+
+const cors = require('cors')
  
 const app = express()
-
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
     graphiql: true,
     schema
